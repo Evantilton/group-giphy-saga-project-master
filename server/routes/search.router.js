87da451adc16e435ba3f5f5f625bf61a07322f7f
evantilton <evantilton@gmail.com>
2019-11-08 14:26:28 -0600

    working on API

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/:search', (req, res) => {
     
     axios({
         method: 'GET',
@@ -10,11 +10,10 @@ router.get('/', (req, res) => {
             api_key: process.env.GIPHY_API_KEY,
             limit: 5,
             offset: 0,
-            // q: SEARCH,
+            q: req.params.search,
             rating: 'g',
         },
-        url: 'http://api.giphy.com/v1/gifs/random'
-        // url: 'api.giphy.com/v1/gifs/search',
+        url: 'api.giphy.com/v1/gifs/search',
     }).then(response => {
         console.log(response);
         res.send(response.data);
