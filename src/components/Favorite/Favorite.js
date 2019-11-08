d4adfd9dfa8de168722364feaff23ec2b7fe11fd
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-08 10:11:06 -0600

    created two components

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Favorite extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Favorites Page!</h1>
+      </div>
+    );
+  }
+  
+}
+
+export default Favorite;
