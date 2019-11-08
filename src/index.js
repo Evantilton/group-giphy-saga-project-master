0f2f9df86a275e56d61cd1aaad08bbdf4e3919d5
Chris Ferber <chris@ferbers.us>
2019-11-08 10:33:12 -0600

    create routes with links from app.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './components/App/App';
 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
